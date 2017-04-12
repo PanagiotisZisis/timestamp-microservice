dcb4e61f135d95f77965baa9cc08f25b5ee20db4
Panagiotis Zisis <takis-zissis@hotmail.com>
2017-04-12 17:49:53 +0000

    Final product

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 var express = require('express');
-var app = express()
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-app.get('/', function (req, res) {
-  res.send('Hello Takis!')
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/:date', function(req, res) {
+   
+   var date = req.params.date;
+   var dateFormattingOptions = {
+       year: 'numeric',
+       month: 'long',
+       day: 'numeric'
+   };
+   
+    if (isNaN(date)) {
+       var naturalDate = new Date(date);
+       naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);
+       var unixDate = new Date(date).getTime()/1000;
+       if (naturalDate == 'Invalid Date') {
+           naturalDate = null;
+           unixDate = null;
+       }
+   } else {
+       var unixDate = date;
+       var naturalDate = new Date(date * 1000)
+       naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);
+       if (naturalDate == 'Invalid Date') {
+           naturalDate = null;
+           unixDate = null;
+       }
+   }
+   
+   res.json({
+       unix: unixDate,
+       natural: naturalDate
+   });
+    
 });
 
 app.listen(8080, function () {
